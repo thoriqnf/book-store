3db59997a2ffd2c322fc51b4437116c4c1859bd4
hafidz <hafidzzpermana09@gmail.com>
2017-11-03 15:53:27 +0700

    initialize backend part

---
+++ backend/app.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(express.static(__dirname+'/client'));
+app.use(bodyParser.json());
+
+Genre =require('./models/genre');
+Book =require('./models/book');
+
+// Connect to Mongoose ------------------------------
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connection;
+
+//Check Connection------------------------------------
+
+app.get('/', (req, res) => {
+	res.send('Please use /api/books or /api/genres');
+});
+
+//GET book GENRES -----------------------------------
+app.get('/api/genres', (req, res) => {
+	Genre.getGenres((err, genres) => {
+		if(err){
+			throw err;
+		}
+		res.json(genres);
+	});
+});
+
+
+///////////////////////////////////////////////////////
+
+
+// GET All Book----------------------------------------
+app.get('/api/books', (req, res) => {
+	Book.getBooks((err, books) => {
+		if(err){
+			throw err;
+		}
+		res.json(books);
+	});
+});
+
+// GET Book by ID--------------------------------------
+app.get('/api/books/:_id', (req, res) => {
+	Book.getBookById(req.params._id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+// POST Book to the List Book-------------------------
+app.post('/api/books', (req, res) => {
+	var book = req.body;
+	Book.addBook(book, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+// UPDATE specific book ------------------------------
+app.put('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	var book = req.body;
+	Book.updateBook(id, book, {}, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+// DELETE specific book -------------------------------
+app.delete('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	Book.removeBook(id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.listen(3000);
+console.log('Running on port 3000...');
